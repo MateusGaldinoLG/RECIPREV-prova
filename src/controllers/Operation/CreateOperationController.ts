8215665dbe69057b9b5fded7e49f972e32cd0dc7
MateusGaldinoLG <mateuslguilherme@gmail.com>
2022-08-01 20:02:14 -0300

    fix cnpj handler

--- src/controllers/Operation/CreateOperationController.ts
+++ src/controllers/Operation/CreateOperationController.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import { CreateOperationService } from "../../services/Operation/CreateOperationService";
 
+function formatAsCPNJ(cnpj: string){
+    let temp = cnpj
+    return temp.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5")
+}
 
 class CreateOperationController{
     async handle(req: Request, res: Response){
@@ -8,11 +12,13 @@ class CreateOperationController{
 
         const {cnpj, razao_social, tipo, num_cotas, valor_unitario, date} = req.body;
 
+        let newCnpj = formatAsCPNJ(cnpj);
+
         if(tipo !== "COMPRA" && tipo !== "VENDA"){
             throw new Error('Tipo invalido')
         }
 
-        const operation = await createOperationService.execute({cnpj, razao_social, tipo, num_cotas, valor_unitario, date});
+        const operation = await createOperationService.execute({cnpj: newCnpj, razao_social, tipo, num_cotas, valor_unitario, date});
 
         return res.status(201).json(operation);
     }
